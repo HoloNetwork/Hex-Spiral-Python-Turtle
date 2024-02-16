ab3102309a66c22bef954771599067ef72c3f7fd
Richard Yitong Zhao <131412198+HoloNetwork@users.noreply.github.com>
2024-02-17 10:01:08 +1300

    Update turtle.py

--- turtle.py
+++ turtle.py
@@ -1,10 +1,20 @@
 import turtle
-colors=['red', 'purple', 'blue', 'green', 'yellow', 'orange']
+
 t = turtle.Pen()
 t.speed(0)
 turtle.bgcolor('black')
+
+colors=['yellow', 'orange', 'red', 'purple', 'blue', 'green']
+max_length = 280
+current_length = 0
+
 for x in range(360):
+    if current_length == max_length:
+        break
     t.pencolor(colors[x%6])
     t.width(x/150)
     t.forward(x)
     t.left(55)
+    current_length += 1
+
+turtle.done()
